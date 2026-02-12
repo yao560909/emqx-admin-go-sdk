256f98f2e1ea7c57af2e53ecd4c84500b7207e7d
yaol06 <yaol06@chinatelecom.cn>
2026-02-12 15:01:07 +0800

    fix:add UnmarshalJSON

--- service/nodes/builder.go
+++ service/nodes/builder.go
@@ -1,6 +1,10 @@
 package nodes
 
-import "github.com/yao560909/emqx-admin-go-sdk/core"
+import (
+	"encoding/json"
+
+	"github.com/yao560909/emqx-admin-go-sdk/core"
+)
 
 type ListNodesReq struct {
 	apiReq *core.APIReq
@@ -9,7 +13,17 @@ type ListNodesReq struct {
 type ListNodesResp struct {
 	*core.APIResp `json:"-"`
 	core.CodeError
-	Data []*Node
+	Data []*Node `json:"-"`
+}
+
+func (resp *ListNodesResp) UnmarshalJSON(b []byte) error {
+	var nodes []*Node
+	if err := json.Unmarshal(b, &nodes); err == nil {
+		resp.Data = nodes
+		return nil
+	}
+	type alias ListNodesResp
+	return json.Unmarshal(b, (*alias)(resp))
 }
 
 type ListNodesReqBuilder struct {
